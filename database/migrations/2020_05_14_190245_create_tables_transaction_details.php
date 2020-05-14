0b488fba82f8c7e0ad71f534d92a52d7e02344b2
unknown <sunarto@smkn1-cirebon.sch.id>
2020-05-14 19:11:21 +0700

    add table transaction_details

--- database/migrations/2020_05_14_190245_create_tables_transaction_details.php
+++ database/migrations/2020_05_14_190245_create_tables_transaction_details.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionsTable extends Migration
+class CreateTablesTransactionDetails extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateTransactionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('transaction_details', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('unit_id');
-            $table->foreignId('reciever')->nullable();
-            $table->longText('notes')->nullable();
+            $table->foreignId('transaction_id');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateTransactionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('tables_transaction_details');
     }
 }
--- database/migrations/2020_05_14_190431_add_foreign_key_on_table_transaction_details.php
+++ database/migrations/2020_05_14_190431_add_foreign_key_on_table_transaction_details.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionsTable extends Migration
+class AddForeignKeyOnTableTransactionDetails extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateTransactionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('unit_id');
-            $table->foreignId('reciever')->nullable();
-            $table->longText('notes')->nullable();
-            $table->timestamps();
+        Schema::table('transaction_details', function (Blueprint $table) {
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');
         });
     }
 
@@ -29,6 +25,6 @@ class CreateTransactionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transactions');
+        //
     }
 }

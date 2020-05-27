e624c29a01fdcb09800e8f3351233efff1193a90
SUNARTO <sunarto@smkn1-cirebon.sch.id>
2020-05-27 14:05:30 +0700

    helper tanggal indonesia

--- app/Helpers/TanggalIndo.php
+++ app/Helpers/TanggalIndo.php
@@ -5,6 +5,6 @@ use Carbon\Carbon;
 function tgl_id($tgl)
 {
     $tgl = new Carbon($tgl);
-    setlocale(LC_ALL, 'id_ID.UTF8', 'id_ID.UTF-8', 'id_ID.8859-1', 'id_ID', 'en_US.UTF8', 'en_US.UTF-8', 'en_US');
+    setlocale(LC_TIME, 'IND');
     return $tgl->formatLocalized('%d %B %Y');
 }
